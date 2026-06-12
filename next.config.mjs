e7665b575bb33902804bbc52b1f3e4531b85a7fb
Potla Deepak <poltadeepak49@gmail.com>
2026-06-12 15:20:16 +0530

    Upgrade to Next.js 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.56.1'],
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
 export default nextConfig;
