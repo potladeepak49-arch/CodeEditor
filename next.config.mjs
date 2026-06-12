f99e0346535f1df9ff5ecc58de83238a7f95f808
Potla Deepak <poltadeepak49@gmail.com>
2026-06-12 14:31:51 +0530

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   allowedDevOrigins: ['192.168.56.1'],
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
